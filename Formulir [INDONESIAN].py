5fb515fb824ca9abaabdad07a6f0403e4fb89eab
Nathan <132364544+Devilpapy@users.noreply.github.com>
2023-05-02 21:03:40 +0700

    Update Formulir [INDONESIAN].py

--- Formulir [INDONESIAN].py
+++ Formulir [INDONESIAN].py
@@ -7,11 +7,11 @@ from tkinter import filedialog
 
 
 window = tk.Tk()
-window.title("Form")
+window.title("Formulir")
 
 window.geometry("400x350")
 
-nama_label = tk.Label(window, text="Fullname:")
+nama_label = tk.Label(window, text="Nama Lengkap:")
 nama_label.grid(row=0, column=0, padx=10, pady=15)
 nama_entry = tk.Entry(window)
 nama_entry.grid(row=0, column=1, padx=10, pady=15)
@@ -19,13 +19,13 @@ nama_entry.grid(row=0, column=1, padx=10, pady=15)
 gender_label = tk.Label(window, text="Gender:")
 gender_label.grid(row=1, column=0, padx=10, pady=10)
 gender_var = tk.StringVar()
-gender_var.set("Man")
-gender_radio1 = tk.Radiobutton(window, text="Man", variable=gender_var, value="Man")
-gender_radio2 = tk.Radiobutton(window, text="Woman", variable=gender_var, value="Woman")
+gender_var.set("Laki-laki")
+gender_radio1 = tk.Radiobutton(window, text="Laki-Laki", variable=gender_var, value="Laki-Laki")
+gender_radio2 = tk.Radiobutton(window, text="Perempuan", variable=gender_var, value="Perempuan")
 gender_radio1.grid(row=1, column=1, padx=10, pady=10)
 gender_radio2.grid(row=1, column=2, padx=10, pady=10)
 
-umur_label = tk.Label(window, text="Age:")
+umur_label = tk.Label(window, text="Umur:")
 umur_label.grid(row=2, column=0, padx=10, pady=10)
 umur_entry = tk.Entry(window)
 umur_entry.grid(row=2, column=1, padx=10, pady=10)
@@ -40,18 +40,18 @@ password_label.grid(row=4, column=0, padx=10, pady=10)
 password_entry = tk.Entry(window, show="*")
 password_entry.grid(row=4, column=1, padx=10, pady=10)
 
-alamat_label = tk.Label(window, text="Addresss:")
+alamat_label = tk.Label(window, text="Alamat:")
 alamat_label.grid(row=5, column=0, padx=10, pady=10)
 alamat_entry = tk.Entry(window)
 alamat_entry.grid(row=5, column=1, padx=10, pady=10)
 
 def submit():
-    print("Full Name:", nama_entry.get())
+    print("Nama Lengkap:", nama_entry.get())
     print("Gender:", gender_var.get())
-    print("Age:", umur_entry.get())
+    print("Umur:", umur_entry.get())
     print("Email:", email_entry.get())
     print("Password:", password_entry.get())
-    print("Address:", alamat_entry.get())
+    print("Alamat:", alamat_entry.get())
 
 
 def save_data():
@@ -68,22 +68,22 @@ def save_data():
     filepath = os.path.join(os.path.expanduser("~"), "Documents", filename)
 
     with open(filepath, 'w') as f:
-        f.write(f"Fullname: {nama}\n")
+        f.write(f"Nama Lengkap: {nama}\n")
         f.write(f"Gender: {gender}\n")
         f.write(f"Umur: {umur}\n")
         f.write(f"Email: {email}\n")
         f.write(f"Password: {password}\n")
-        f.write(f"Address: {alamat}\n")
+        f.write(f"Alamat: {alamat}\n")
 
-        messagebox.showinfo("Save Data", "Data saved successfully!!")
+        messagebox.showinfo("Simpan Data", "Data berhasil disimpan!!")
 
 
-submit_button = tk.Button(window, text="Submit", bg = "red", fg = "white", command=submit)
+submit_button = tk.Button(window, text="Kirimkan", bg = "red", fg = "white", command=submit)
 submit_button.grid(row=6, column=1, padx=10, pady=10)
 
-tk.Button(window, text="Save Data", command=save_data)
+tk.Button(window, text="Simpan Data", command=save_data)
 
-save_button = tk.Button(window, text="Save Data", command=save_data)
+save_button = tk.Button(window, text="Simpan Data", command=save_data)
 save_button.grid(row=7, column=1, padx=10, pady=10)
 
 window.mainloop()
